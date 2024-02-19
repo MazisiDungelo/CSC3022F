bc7c341cca9b867492f941d2d2b2801ddb16fd2b
MazisiDungelo <dngmaz001@myuct.ac.za>
2024-02-19 21:22:11 +0200

    Implemented driver file

--- Assignment_1/myWCdriver.cpp
+++ Assignment_1/myWCdriver.cpp
@@ -1,11 +1,11 @@
 #include <iostream>   //a system header file - needed to do simple I/O
 #include "myWC.h"	         //header file - contains decl of fib()
 
-using namespace std;  
-
-
 int main (void)
 {
-	
+      using namespace DNGMAZ001;
+	std::cout << "Please enter a sentence" << std::endl;
+      std::cout << countLines() << " " << countWords()  <<  " " << countChars() << std::endl;
+
       return 0; 
 }
