26598280e976cf37905455c66f8c54e9ba4ee397
MazisiDungelo <dngmaz001@myuct.ac.za>
2024-02-19 21:20:00 +0200

    Implemented myWC header file, declared all variables and functions

--- Assignment_1/myWC.h
+++ Assignment_1/myWC.h
@@ -1,9 +1,35 @@
 // myWC.h - header file for my word count application
 
-#ifndef _MYWC
-#define _MYWC
+#ifndef _MYWC_H
+#define _MYWC_H
 
-int count(int n);   // a function "prototype" - tells compiler all about count()       
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace DNGMAZ001{
+    extern int noOflines;
+    extern std::vector<std::string> lines;
+
+    extern int noOfwords;
+    extern std::vector<std::string> words;
+
+    extern int noOfchars;
+    extern std::vector<std::string> chars;
+
+    extern std::vector<char> specCharacters;
+
+    int countLines(); // function to count the lines that are in text 
+
+    int countWords(); // function to count words in a sentence
+
+    int countChars(); // function to count characcter in a word
+
+    bool isSpecChar(char ch); // function to check is character ch is a special character or not
+
+}
+
+     
 
 #endif
 
