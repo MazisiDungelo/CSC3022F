6eba922660f55482dedaddb564707a0b76e778ef
MazisiDungelo <dngmaz001@myuct.ac.za>
2024-03-09 18:10:52 +0200

    Added header(declaration) for Tile class

---
+++ Assignment_2/Tile.h
@@ -0,0 +1,47 @@
+#ifndef TILE_H
+#define TILE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+namespace DNGMAZ001{
+
+        class Tile{
+            private:
+                int x;
+                int y;
+                int width;
+                int height;
+                unsigned char** pixels;
+            
+            public:
+                // Default constructor 
+                Tile();
+                // Custom constructor
+                Tile(int x, int y,int width, int height, unsigned char** inputPixels);
+                // Destructor
+                ~Tile();
+                // Copy Constructor
+                Tile(const Tile& other);
+
+                // Copy Assignment Operator
+                Tile& operator=(const Tile& other);
+
+                // Move Constructor
+                Tile(Tile&& other) noexcept;
+
+                // Move Assignment Operator
+                Tile& operator=(Tile&& other) noexcept;
+
+
+                unsigned char getPixel(int row, int col) const;
+                unsigned char** getPixels() const;
+                int getWidth() const;
+                int getHeight() const;
+                void makeBlank();
+
+    };
+}
+
+#endif
