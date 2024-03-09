7ae3e8a3be954972f2b81ebb2ae7edc54341eaaf
MazisiDungelo <dngmaz001@myuct.ac.za>
2024-03-09 23:35:12 +0200

    Added command line interaction code

--- Assignment_2/driver.cpp
+++ Assignment_2/driver.cpp
@@ -3,18 +3,20 @@
 
 int main(int argc, char* argv[]) {
     // Set the grid size, image name, and number of moves
-    int gridSize = 3; 
-    std::string imageName = "mona512.pgm"; 
-    int numMoves = 2; 
 
+    int gridSize, numMoves;
+    std::string imageName;
     // Parse command line arguments
     if (argc == 4) {
         gridSize = std::atoi(argv[1]);
         numMoves = std::atoi(argv[2]);
         imageName = argv[3];
     } else {
-        std::cerr << "Usage: " << argv[0] << " <gridSize> <numMoves> <inputImageName>\n";
-        return 1;  // Return error code
+            gridSize = 3; 
+            imageName = "mona512.pgm"; 
+            numMoves = 2; 
+        // std::cerr << "Usage: " << argv[0] << " <gridSize> <numMoves> <inputImageName>\n";
+        // return 1;  // Return error code
     }
     // Create a TileManager object
     DNGMAZ001::TileManager tileManager(gridSize, imageName, numMoves);
